93497c3865032c4523e18ad438bcfeb774df4ff6
Datum Geek <mike.graham@ihsmarkit.com>
2016-10-25 03:01:30 -0600

    added bootstrap styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [ require('bootstrap/dist/css/bootstrap.css'),
+    `
+      .heading {
+          background-color: cadetblue;
+          margin: 0;
+          padding: 0;
+      }
+
+      .body {
+          background-color: orange;
+      }
+    `
+  ]
 })
 export class AppComponent {
   title = 'app works!';
