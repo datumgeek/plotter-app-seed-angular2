24e21433b70a70c4c3c90e95b18659020c8d5142
Datum Geek <mike.graham@ihsmarkit.com>
2016-11-08 12:01:35 -0700

    wind farm top ten is now launching wind farm details when a wind farm is clicked !! :)

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,8 @@ var exportedModules = {
   "@angular/platform-browser": require('@angular/platform-browser'),
   "@angular/platform-browser-dynamic": require('@angular/platform-browser-dynamic'),
   "@angular/http": require('@angular/http'),
-  "@angular/router": require('@angular/router')
+  "@angular/router": require('@angular/router'),
+  "plotter-shell-angular2/dist/index": require('plotter-shell-angular2/dist/index')
 };
 
 // externally "define" (in the requirejs / dojo sense) the modules that are exported
